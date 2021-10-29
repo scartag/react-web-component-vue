a5616a04b84f431c17f27c7152204b4d32f556c7
Taghogho Emefeke <tag@temlogic.com>
2021-10-29 13:56:20 +0100

    Web component sample

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app" class="mt-0"  style="height: 100vh;">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-3 p-5" style="background: #f0f0f0;">
+          <div class="form-group mb-5">
+            <label>Title / Message</label>
+            <input type="text" class="form-control" v-model="message">
+          </div>
+          <div class="form-group mb-5">
+            <label>Background Color</label>
+            <select class="form-control" v-model="selectedColor">
+              <option v-for="(item,index) in colors" :key="index" :value="item.value">{{item.label}}</option>
+            </select>
+          </div>
+
+        </div>
+        <div class="col-md-9">
+          <test-app :color="selectedColor" :msg="message"></test-app>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data() {
+    return {
+      selectedColor: 'transparent',
+      message: '',
+      colors:  [{value: 'transparent', label:'Transparent'}, {value: 'blue', label: 'Blue'}, {value: 'yellow', label: 'Yellow'}, {value: 'pink', label: 'Pink'}]
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
